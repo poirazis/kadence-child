997f6d8996f26b16c47c96ab043da59474806014
poirazis <poirazis@gmail.com>
2022-03-08 15:33:34 +0200

    Team Member Timeline Activity

--- functions.php
+++ functions.php
@@ -1,22 +1,24 @@
 <?php
 /**
- * Enqueue child styles.
+ * Proto Campaing Child Theme
+ * @version 1.0.0
  */
 
 include_once( get_stylesheet_directory() . '/templates/template-tags.php' );
 include_once( get_stylesheet_directory() . '/helpers/helpers.php' );
 
-function child_enqueue_styles() {
+/**
+ * Enqueue Styles
+ */
+
+ function proto_child_enqueue_styles() {
 	wp_enqueue_style( 'child-theme', get_stylesheet_directory_uri() . '/style.css', array(), 100 );
 }
 
-add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' ); // Remove the // from the beginning of this line if you want the child theme style.css file to load on the front end of your site.
+add_action( 'wp_enqueue_scripts', 'proto_child_enqueue_styles' );
 
-function organic_origin_gutenberg_styles() {
+function proto_child_enqueue_gutenberg_styles() {
 	wp_enqueue_style( 'child-theme', get_theme_file_uri( '/editor-style.css' ), false, '1.0', 'all' );
 }
 
-add_action( 'enqueue_block_editor_assets', 'organic_origin_gutenberg_styles' );
-/**
- * Add custom functions here
- */
+add_action( 'enqueue_block_editor_assets', 'proto_child_enqueue_gutenberg_styles' );
--- templates/template-tags.php
+++ templates/template-tags.php
@@ -29,6 +29,85 @@ function the_team_member_card (int $member_id) {
     }
 }
 
+
+/**
+ * Generate the team member vertical activity timeline
+ * 
+ * 
+ */
+
+function the_team_member_activity (int $member_id) {
+
+    $query_args = array(
+        'post_type' => array('post', 'policy_proposal'),
+        'post_status' => 'publish',
+        'order' => 'DESC',
+        'orderby' => 'date',
+        'posts_per_page' => '5',
+        'meta_query' => array(
+            '0' => array(
+                'key' => 'contributors',
+                'value' => $member_id,
+                'compare' => 'IN',
+            ),
+            'relation' => 'AND',
+        ),
+    );
+
+
+    // The Query
+    $the_query = new WP_Query( $query_args );
+
+    // The Loop
+    if ( $the_query->have_posts() ) {
+        echo '<section id="cd-timeline" class="cd-container">';
+
+        while ( $the_query->have_posts() ) {
+            $the_query->the_post();
+            $the_terms = null;
+            $the_terms_html = null;
+
+            if (get_post_type() === 'policy_proposal') { 
+                $the_terms = get_the_terms( $post->ID, 'polsector', '' , ' | ' );     
+            } else {
+                $the_terms = get_the_terms ($post->ID, 'category', '', ' ', '');
+            }
+
+            if ($the_terms) {
+
+                $the_terms_html = '<div>';
+                foreach ($the_terms as $term) {
+                    $the_terms_html .= '<spanl class="pc-tax-pill">' . $term->name . '</span>';
+                }
+                $the_terms_html .= '</div>';
+            }
+
+            ?>
+
+            <div class="cd-timeline-block">
+                <div class="cd-timeline-img cd-picture">
+                </div>
+
+                <div class="cd-timeline-content">
+                    <h5 class="posted_on"> <?php echo get_the_date('F j, Y'); ?> </h5>
+                    <?php echo $the_terms_html; ?> 
+                    <a href="<?php the_permalink(); ?>"><h2> <?php the_title(); ?> </h2> </a>
+                    <p> <?php the_excerpt(); ?></p>
+                </div> <!-- cd-timeline-content -->
+            </div> <!-- cd-timeline-block -->
+            
+
+        <?php
+        }
+
+        echo '</section>';
+        /* Restore original Post Data */
+        wp_reset_postdata();
+    } else {
+        // no posts found
+    }
+}
+
 function the_policy_proposal_card (int $pp_id, bool $show_tags = true) 
 {
     $mypods = pods('policy_proposal', $pp_id);
